5d3458a812399c9f7e67041b67ad000e7ccf1823
Damilare Ademeso <damilare.ademeso@cardinalstone.com>
2020-05-03 22:51:58 +0100

    radio completed

---
+++ src/Announcement.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Announcement() {
+    return (
+        <div id="announcement">
+            <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                </ol>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <h5>Send Praise reports, Testimonies and Prayer requests to livingwaters@zijela.com, +2348136776626</h5>
+                    </div>
+                    <div className="carousel-item">
+                        <h5>Join us daily for a time of prayer, praise and reflection in God's word. you can as well download the living waters radio app on the android play store</h5>
+                    </div>
+                </div>
+                <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,126 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
+import { Button } from '@material-ui/core';
+import Announcement from './Announcement.jsx';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
 
 function App() {
+  var lwmAudio = useRef();
+  var stream = useRef();
+  const [vol, setVol] = useState(1);
+  const [canPlay, setCanPlay] = useState(false);
+  const [plause, setPlause] = useState("Play");
+  let volInterval = 0.1;
+
+  const increaseVolFn = () => {
+    if (vol < 1) {
+      let sum = vol + volInterval;
+      let roundedSum = Math.round(sum * 10) / 10;
+      if (roundedSum <= 1) {
+        setVol(roundedSum)
+      } else {
+        setVol(1)
+      }
+    }
+    if (vol === 1) {
+      setVol(1);
+    }
+  }
+
+  const decreaseVolFn = () => {
+    let diff = vol - volInterval;
+    let roundedDiff = Math.round(diff * 10) / 10;
+    if (vol > 0) {
+      if (roundedDiff >= 0) {
+        setVol(roundedDiff)
+      } else {
+        setVol(0)
+      }
+    }
+  }
+
+  useEffect(
+    () => {
+      lwmAudio.current.addEventListener("playing", () => {
+        setPlause("Stop");
+      });
+      lwmAudio.current.addEventListener("pause", () => {
+        setPlause("Play");
+      });
+      lwmAudio.current.addEventListener('canplay', () => {
+        setCanPlay(true)
+      });
+      lwmAudio.current.volume = vol;
+    }
+  );
+
+  const playPause = () => {
+    if (plause === "Play") {
+      if (!canPlay) {
+        lwmAudio.current.load();
+        lwmAudio.current.play();
+      } else {
+        lwmAudio.current.play();
+      }
+    }
+    if (plause === "Stop") {
+      lwmAudio.current.pause();
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="audio-container">
+      <div id="audio-wrapper">
+        <img src="/lwm.jpg" style={{ borderRadius: "15px" }} width="100%" height="250px" alt="player" />
+        {
+          !canPlay ?
+            <div style={{
+              width: "100%", backgroundColor: "red", color: "white", padding: "10px",
+              borderRadius: "8px",
+              margin: "10px 0"
+            }}>
+              "Stream currently unavailable. Please try again later"
+            </div>
+            : null
+        }
+        <div style={{
+          display: "flex",
+          flexBasis: "100%",
+          alignItems: "center"
+        }}>
+          <Button variant="outlined" color="primary" onClick={playPause} value={plause}>
+            {plause}
+          </Button>
+          <div style={{
+            flexGrow: 1
+          }}></div>
+          <div style={{
+            textAlign: "center"
+          }}>
+            <div>Volume</div>
+            <div style={{
+              display: "flex"
+            }}>
+              <Button variant="contained" color="primary" onClick={decreaseVolFn}>
+                <RemoveIcon />
+              </Button>
+              <div style={{
+                width: "30px"
+              }}>
+                {vol}
+              </div>
+              <Button variant="contained" color="primary" onClick={increaseVolFn}>
+                <AddIcon />
+              </Button>
+            </div>
+          </div>
+        </div>
+        <audio preload="metadata" ref={lwmAudio} style={{ width: "100%", height: "100%" }}>
+          <source ref={stream} src="http://178.32.107.151:2331/;stream/1" type="audio/mpeg" />
+        </audio>
+        <Announcement />
+      </div>
     </div>
   );
 }
